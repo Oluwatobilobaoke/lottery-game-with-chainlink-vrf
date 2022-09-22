1172ce1270e6231b8145e8dacd146782472c8ade
Pheranmie <tobi.okekunle@reventtechnologies.com>
2022-09-22 14:52:05 +0100

    added graph

---
+++ bot.js
@@ -0,0 +1,10 @@
+function printMyName(name) {
+  const myName = name;
+  const company = "Dyson Protocol";
+  const compliment = `${company} is the best!`;
+  const statment = `${myName} is testing ${company}\n${compliment} !`;
+
+  console.log(statment);
+}
+
+printMyName("whydeeheem");
